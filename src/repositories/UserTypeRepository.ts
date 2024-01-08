4a73bbe221580b844cd2bfee4db0d676f03ed4bc
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-01-07 23:25:01 -0300

    feat: Adding UserTypeRepository

---
+++ src/repositories/UserTypeRepository.ts
@@ -0,0 +1,52 @@
+import { CreateUserTypeInterface } from '../interfaces/user-type/CreateUserTypeInterface';
+import { GetUserTypeInterface } from '../interfaces/user-type/GetUserTypeInterface';
+import { UpdateUserTypeInterface } from '../interfaces/user-type/UpdateUserTypeInterface';
+
+import { prisma } from '../libs/prisma';
+
+class UserRepositoryClient {
+  async findAll() {
+    const user_types = await prisma.userType.findMany();
+
+    return user_types;
+  }
+
+  async create ({ name }: CreateUserTypeInterface) {
+    await prisma.userType.create({
+      data: {
+        name
+      }
+    });
+  }
+
+  async findById({ user_type_id }: GetUserTypeInterface) {
+    const user_type = await prisma.userType.findUnique({
+      where: {
+        id: user_type_id
+      }
+    });
+
+    return user_type;
+  }
+
+  async update({ name, user_type_id }: UpdateUserTypeInterface) {
+    await prisma.userType.update({
+      where: {
+        id: user_type_id
+      },
+      data: {
+        name
+      }
+    });
+  }
+
+  async delete({ user_type_id }: GetUserTypeInterface) {
+    await prisma.userType.delete({
+      where: {
+        id: user_type_id
+      }
+    });
+  }
+}
+
+export default new UserRepositoryClient();
