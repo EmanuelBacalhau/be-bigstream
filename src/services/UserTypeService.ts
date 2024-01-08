6f7997e6bd81604ce68e5f0e554ba1a5a13bcd86
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-01-08 00:09:06 -0300

    feat: Adding method in UserTypeService for create user type

--- src/services/UserTypeService.ts
+++ src/services/UserTypeService.ts
@@ -1,9 +1,19 @@
+import { MyError } from '../errors/MyError';
+import { CreateUserTypeInterface } from '../interfaces/user-type/CreateUserTypeInterface';
 import UserTypeRepository from '../repositories/UserTypeRepository';
 
 class UserTypeService {
   async index() {
     const user_types = await UserTypeRepository.findAll();
     return user_types;
+  }
+
+  async store({ name }: CreateUserTypeInterface) {
+    const existsUserType = await UserTypeRepository.findByName({ name });
+
+    if (existsUserType) throw new MyError('User type already exists', 409);
+
+    await UserTypeRepository.create({ name });
   }
 }
 
