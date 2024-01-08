d95a01404436b49a8c0a4594eebf4cf3af68ce22
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-01-08 00:28:47 -0300

    feat: Adding router for show user types. GET - /user-types/:id

--- src/routes.ts
+++ src/routes.ts
@@ -6,3 +6,4 @@ export const router = Router();
 // UserType
 router.get('/user-types', UserTypeController.index);
 router.post('/user-types', UserTypeController.store);
+router.get('/user-types/:id', UserTypeController.show);
