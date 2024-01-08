1e210b2bc5f6ba106dcce9550c7b94937f59d23a
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-01-08 00:09:17 -0300

    feat: Adding method in UserTypeController for create user type

--- src/controllers/UserTypeController.ts
+++ src/controllers/UserTypeController.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from 'express';
 import UserTypeService from '../services/UserTypeService';
+import { z } from 'zod';
 
 class UserTypeController {
   async index(req: Request, res: Response) {
     const user_types = await UserTypeService.index();
 
     return res.status(200).json(user_types);
+  }
+
+  async store(req: Request, res: Response) {
+    const UserTypeSchema = z.object({
+      name: z.string().min(1)
+    });
+
+    const { name } = UserTypeSchema.parse(req.body);
+
+    await UserTypeService.store({ name });
+
+    return res.status(201).end();
   }
 }
 
