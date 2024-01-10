ccba54a87753fc9210c645d60e3f7be893faf748
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-01-09 23:54:11 -0300

    feat: Adding router for find all user. GET - /users

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import UserTypeController from './controllers/UserTypeController';
+import UserController from './controllers/UserController';
 
 export const router = Router();
 
@@ -9,3 +10,6 @@ router.post('/user-types', UserTypeController.store);
 router.get('/user-types/:id', UserTypeController.show);
 router.patch('/user-types/:id', UserTypeController.update);
 router.delete('/user-types/:id', UserTypeController.delete);
+
+// User
+router.get('/users', UserController.index);
