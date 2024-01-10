73eb382e6cf5b25766e86af52eb3d75c137d0c64
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-01-10 11:28:20 -0300

    feat: Adding method in UserService for update user

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,6 +1,7 @@
 import { MyError } from '../errors/MyError';
 import { CreateUserInterface } from '../interfaces/user/CreateUserInterface';
 import { GetUserInterface } from '../interfaces/user/GetUserInterface';
+import { UpdateUserInterface } from '../interfaces/user/UpdateUserInterface';
 import UserRepository from '../repositories/UserRepository';
 import UserTypeRepository from '../repositories/UserTypeRepository';
 
@@ -33,6 +34,27 @@ class UserService {
     if (!user) throw new MyError('User not found', 200);
 
     return user;
+  }
+
+  async update({user_id, ...data}: UpdateUserInterface) {
+
+    const userExists = await UserRepository.findUnique({ user_id });
+
+    if (!userExists) throw new MyError('User not found', 200);
+
+    const userTypeExists = data.user_type_id && await UserTypeRepository.findByIdOrName({ user_type_id: data.user_type_id });
+
+    if (data.user_type_id && !userTypeExists) throw new MyError('User type not exists', 409);
+
+    const emailInUse = data.email && await UserRepository.findUnique({ email: data.email });
+
+    if (data.email && emailInUse) throw new MyError('Is email in use', 409);
+
+    const phoneInUse = data.phone && await UserRepository.findUnique({ phone: data.phone });
+
+    if (data.phone && phoneInUse) throw new MyError('Is phone in use', 409);
+
+    await UserRepository.update({user_id, ...data});
   }
 }
 
