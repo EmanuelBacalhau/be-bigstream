d7e8eb484b3bb6c2ce51f13c4f5d0642d134d8e1
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-01-11 14:48:25 -0300

    feat: Adding method in TypeOfTrainingService for update type of training

--- src/services/TypeOfTrainingService.ts
+++ src/services/TypeOfTrainingService.ts
@@ -1,6 +1,7 @@
 import { MyError } from '../errors/MyError';
 import { CreateTypeOfTrainingInterface } from '../interfaces/typeOfTraining/CreateTypeOfTrainingInterface';
 import { GetTypeOfTrainingInterface } from '../interfaces/typeOfTraining/GetTypeOfTrainingInterface';
+import { UpdateTypeOfTrainingInterface } from '../interfaces/typeOfTraining/UpdateTypeOfTrainingInterface';
 import TypeOfTrainingRepository from '../repositories/TypeOfTrainingRepository';
 
 class TypeOfTrainingService {
@@ -14,13 +15,24 @@ class TypeOfTrainingService {
     await TypeOfTrainingRepository.create({ name });
   }
 
-
   async show({ type_id }: GetTypeOfTrainingInterface) {
     const type = await TypeOfTrainingRepository.findUnique({ type_id });
 
     if (!type) throw new MyError('Type of training not found', 200);
 
     return type;
+  }
+
+  async update({ type_id, name }: UpdateTypeOfTrainingInterface) {
+    const typeExists = await TypeOfTrainingRepository.findUnique({ type_id });
+
+    if (!typeExists) throw new MyError('Type of training not found', 200);
+
+    const nameInUse = await TypeOfTrainingRepository.findUnique({ name });
+
+    if (nameInUse) throw new MyError('Name in use', 409);
+
+    await TypeOfTrainingRepository.update({ name, type_id });
   }
 }
 
