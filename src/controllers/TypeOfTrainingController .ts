b0804b5a8f8dcca848a379fb39ed3afdf71e4d26
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-01-11 14:27:03 -0300

    feat: Adding method in TypeOfTrainingController for create type of training

--- src/controllers/TypeOfTrainingController .ts
+++ src/controllers/TypeOfTrainingController .ts
@@ -1,11 +1,24 @@
 import { Request, Response } from 'express';
 import TypeOfTrainingService from '../services/TypeOfTrainingService';
+import { z } from 'zod';
 
 class TypeOfTrainingController {
   async index(req: Request, res: Response) {
     const types = await TypeOfTrainingService.index();
 
     return res.status(200).json(types);
+  }
+
+  async store(req: Request, res: Response) {
+    const TypeOfTrainingSchema = z.object({
+      name: z.string().min(1)
+    });
+
+    const { name } = TypeOfTrainingSchema.parse(req.body);
+
+    await TypeOfTrainingService.store({ name });
+
+    return res.status(201).end();
   }
 }
 
